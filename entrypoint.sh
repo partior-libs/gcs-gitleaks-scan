356406b4fc75ae93a52c1843e23a56c88141ae56
Anthony Yeong <anthony.yeong@partior.com>
2022-09-15 09:40:02 +0800

    DSO-970 Try update permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,14 @@ echo running gitleaks "$(gitleaks version) with the following command👇"
 
 DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring me or donating a little something\n\e[36mhttps://github.com/sponsors/zricethezav\n\e[36mhttps://www.paypal.me/zricethezav\n"
 echo [DEBUG] Listing...
-ls -ltrah $GITHUB_WORKSPACE
+git config --add safe.directory /github/workspace
+# ls -ltrah $GITHUB_WORKSPACE
 echo [DEBUG] Git config...
-ls -ltrah /github/home/.gitconfig
-ls -ltrah /github/home/
-git config --global --add safe.directory /github/workspace
-git version
-git config --global --get-all safe.directory
+# ls -ltrah /github/home/.gitconfig
+# ls -ltrah /github/home/
+# git config --global --add safe.directory /github/workspace
+# git version
+git config --get-all safe.directory
 echo [DEBUG] Git config... end
 if [ "$GITHUB_EVENT_NAME" = "push" ]
 then
