0e8d3faa510794d0205115734c1f42d264e60888
Anthony Yeong <anthony.yeong@partior.com>
2022-09-15 09:01:45 +0800

    DSO-970 Check git version

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,7 @@ DONATE_MSG="👋 maintaining gitleaks takes a lot of work so consider sponsoring
 echo [DEBUG] Listing...
 ls -ltrah $GITHUB_WORKSPACE
 echo [DEBUG] Git config...
+git version
 git config --global --get-all safe.directory
 echo [DEBUG] Git config... end
 if [ "$GITHUB_EVENT_NAME" = "push" ]
